beb85d2b1908eb2d1aa2caf157af6e79bf6f7a31
EKomin220 <ifa.komin@gmail.com>
2025-05-03 19:16:34 +0100

    add hamburger menu

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+function openMenu(){
+    const menuLinks = document.getElementById("menu-links")
 
+    if (menuLinks.style.display==="block"){
+        menuLinks.style.display="none"
+    } else{
+        menuLinks.style.display="block"
+    }
+}
